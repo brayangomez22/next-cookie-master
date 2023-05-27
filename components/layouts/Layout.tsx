60d0b03351259e0a28ae97875fd5f3901e819017
Brayan Gómez <brayan.gomez@sofka.com.co>
2023-05-27 16:10:46 -0500

    first commit

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import Head from "next/head";
+import { Navbar } from "../ui";
+
+export const Layout: FC = ({ children }) => {
+  return (
+    <>
+      <Head></Head>
+      <nav>
+        <Navbar />
+      </nav>
+      <main style={{ padding: "20px 50px" }}>{children}</main>
+    </>
+  );
+};
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Layout } from "../components/layouts";
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Cookie Master</h1>
+    </Layout>
+  );
+};
+
+export default Home;
